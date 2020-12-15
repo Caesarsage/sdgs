f9464673b7985473689c8693b68fcc0684a739ec
Destiny Erhabor <destinterhabor6@gmail.com>
2020-12-15 12:49:06 +0100

    sdgs web applications

---
+++ src/scripts/app.js
@@ -0,0 +1,102 @@
+// const sdgData = [
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 2,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal2.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal3.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+//   {
+//     id: 1,
+//     imgUrl: 'assets/Goals/goal1.png',
+//     description: 'lorew',
+//   },
+// ];
+// let cardsdg = document.querySelector('#cardSdg');
+
+// sdgData.forEach(data => {
+//   const img = document.createElement('img');
+//   const div = document.createElement('div');
+//   const i = document.createElement('li');
+
+//   img.innerHTML =`<img src=${data.imgUrl} alt="goal1" class="card-img-top">`
+
+
+//   cardsdg.append(img);
+//   img.append(div);
+//   div.appendChild(i)
+
+  
+//   console.log(data);
+// });
+
+// console.log(cardsdg.children[0].attributes[0].value);
+// console.log(cardsdg.children[0].attributes[0].value);  
